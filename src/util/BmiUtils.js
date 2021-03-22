841bed2773f4baf99acb2b1dd2a6e5782f48f71f
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 11:40:55 -0600

    refactored to reduce redundancy

--- src/util/BmiUtils.js
+++ src/util/BmiUtils.js
@@ -1,64 +1,61 @@
 const thresholds = {
-  underweight: 18.5,
-  normal: 25,
-  overweight: 30,
+  underweight: 18.49,
+  normal: 24.99,
+  overweight: 29.99,
 };
 
 const delimitThresholds = 0.0000001;
-
-export const BmiUtils = {
-  calculateBmi: (kg, meters) => kg / (meters * meters),
-  bmiCategory: (bmi) => {
-    if (bmi < categories.underweight.upperThreshold)
-      return categories.underweight;
-    else if (bmi < categories.normal.upperThreshold) return categories.normal;
-    else if (bmi < categories.overweight.upperThreshold)
-      return categories.overweight;
-    else return categories.obese;
-  },
-  underweightThresholdKg: (metersTall) => ({
-    min: undefined,
-    max: metersTall * metersTall * thresholds.normal - delimitThresholds,
-  }),
-  normalThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * thresholds.normal,
-    max: metersTall * metersTall * thresholds.overweight,
-  }),
-  overweightThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * thresholds.overweight + delimitThresholds,
-    max: metersTall * metersTall * thresholds.overweight,
-  }),
-  obeseThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * thresholds.overweight + delimitThresholds,
-    max: undefined,
-  }),
-};
-
 export const categories = {
   underweight: {
     name: "underweight",
-    lowerThreshold: 0,
-    upperThreshold: 18.5,
+    minBmi: 0,
+    maxBmi: thresholds.underweight,
     percent: 0,
   },
   normal: {
     name: "normal",
-    lowerThreshold: 18.5,
-    upperThreshold: 24.9,
+    minBmi: thresholds.underweight + delimitThresholds,
+    maxBmi: thresholds.normal,
     percent: 0.25,
   },
   overweight: {
     name: "overweight",
-    lowerThreshold: 24.9,
-    upperThreshold: 29.9,
+    minBmi: thresholds.normal + delimitThresholds,
+    maxBmi: thresholds.overweight,
     percent: 0.75,
   },
   obese: {
     name: "obese",
-    lowerThreshold: 29.9,
-    upperThreshold: 100,
+    minBmi: thresholds.overweight + delimitThresholds,
+    maxBmi: 100,
     percent: 1,
   },
+};
+
+export const BmiUtils = {
+  calculateBmi: (kg, meters) => kg / (meters * meters),
+  bmiCategory: (bmi) => {
+    if (bmi < thresholds.underweight) return categories.underweight;
+    else if (bmi < thresholds.normal) return categories.normal;
+    else if (bmi < thresholds.overweight) return categories.overweight;
+    else return categories.obese;
+  },
+  underweightThresholdKg: (metersTall) => ({
+    min: undefined,
+    max: metersTall * metersTall * categories.underweight.maxBmi,
+  }),
+  normalThresholdKg: (metersTall) => ({
+    min: metersTall * metersTall * categories.normal.minBmi,
+    max: metersTall * metersTall * categories.normal.maxBmi,
+  }),
+  overweightThresholdKg: (metersTall) => ({
+    min: metersTall * metersTall * categories.overweight.minBmi,
+    max: metersTall * metersTall * categories.overweight.maxBmi,
+  }),
+  obeseThresholdKg: (metersTall) => ({
+    min: metersTall * metersTall * categories.obese.minBmi,
+    max: undefined,
+  }),
 };
 
 export const convert = {
