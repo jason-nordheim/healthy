4b04a44d3a9491a30c2a892fdfe7a39b4a24c8e9
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 15:36:10 -0600

    added the <NavBar />, and executed basic setup of <BrowserRouter /> component for client-side routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BmiCalculator } from "./components/BmiCalculator";
+import { NavBar } from "./components/layout/nav/NavBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BmiCalculator />
+    <NavBar />
+    <Router>
+      <Route to="/">
+        <BmiCalculator />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
