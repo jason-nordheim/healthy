a7cce2e66803c52488a54b79e2c3e18f9de6c5c6
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 15:35:06 -0600

    created <Brand /> component

---
+++ src/components/layout/nav/Brand.js
@@ -0,0 +1,20 @@
+export const Brand = () => {
+  return (
+    <>
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </>
+  );
+};
