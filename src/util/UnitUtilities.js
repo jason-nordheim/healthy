face3eda08a9e91faf757ab4504beb83417fc97f
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 12:15:10 -0600

    created UnitUtilities.js to hold functions realted to converting numbers

---
+++ src/util/UnitUtilities.js
@@ -0,0 +1,6 @@
+export const convert = {
+  inchesToCentimeters: (inches) => inches * 2.54,
+  centimetersToInches: (centimeters) => centimeters / 2.54,
+  poundsToKilograms: (pounds) => pounds / 2.205,
+  kilogramsToPounds: (kilograms) => kilograms * 2.205,
+};
