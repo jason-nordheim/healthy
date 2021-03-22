a5b00b79d320dbb24a0c16105968a0c75fe5ce31
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 12:14:50 -0600

    added comments and refactored the convert object into a seperate file

--- src/util/BmiUtils.js
+++ src/util/BmiUtils.js
@@ -1,3 +1,5 @@
+import { convert } from "./UnitUtilities";
+
 const thresholds = {
   underweight: 18.49,
   normal: 24.99,
@@ -5,6 +7,10 @@ const thresholds = {
 };
 
 const delimitThresholds = 0.0000001;
+
+/**
+ * Define the different BMI categories
+ */
 export const categories = {
   underweight: {
     name: "underweight",
@@ -32,35 +38,97 @@ export const categories = {
   },
 };
 
-export const BmiUtils = {
-  calculateBmi: (kg, meters) => kg / (meters * meters),
-  bmiCategory: (bmi) => {
-    if (bmi < thresholds.underweight) return categories.underweight;
-    else if (bmi < thresholds.normal) return categories.normal;
-    else if (bmi < thresholds.overweight) return categories.overweight;
-    else return categories.obese;
-  },
-  underweightThresholdKg: (metersTall) => ({
-    min: undefined,
-    max: metersTall * metersTall * categories.underweight.maxBmi,
-  }),
-  normalThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * categories.normal.minBmi,
-    max: metersTall * metersTall * categories.normal.maxBmi,
-  }),
-  overweightThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * categories.overweight.minBmi,
-    max: metersTall * metersTall * categories.overweight.maxBmi,
-  }),
-  obeseThresholdKg: (metersTall) => ({
-    min: metersTall * metersTall * categories.obese.minBmi,
-    max: undefined,
-  }),
+const weightRangeLb = (height, category) => {
+  const kgRange = weightRangeKg(height, category);
+  return {
+    min: convert.kilogramsToPounds(kgRange.min),
+    max: convert.kilogramsToPounds(kgRange.max),
+  };
 };
 
-export const convert = {
-  inchesToCentimeters: (inches) => inches * 2.54,
-  centimetersToInches: (centimeters) => centimeters / 2.54,
-  poundsToKilograms: (pounds) => pounds / 2.205,
-  kilogramsToPounds: (kilograms) => kilograms * 2.205,
+/**
+ * Calculates the minumum and maximum weight in kilograms for each BMI category
+ * @param {Number} height (meters)
+ * @returns
+ */
+const weightRangeKg = (height, category) => {
+  switch (category) {
+    case categories.underweight.name:
+      return {
+        min: undefined,
+        max: height * height * categories.underweight.maxBmi,
+      };
+    case categories.normal.name:
+      return {
+        min: height * height * categories.normal.minBmi,
+        max: height * height * categories.normal.maxBmi,
+      };
+    case categories.overweight.name:
+      return {
+        min: height * height * categories.overweight.minBmi,
+        max: height * height * categories.overweight.maxBmi,
+      };
+    case categories.obese.name:
+      return {
+        min: height * height * categories.obese.minBmi,
+        max: undefined,
+      };
+    default:
+      throw new Error(`Invalid Category: ${category}`);
+  }
+};
+
+/**
+ * Determines the range (min/max) for each BMI category
+ * @param {Number} height in meters
+ * @returns object containing the ranges for each BMI category
+ */
+const kgRange = (height) => {
+  return {
+    underweight: weightRangeKg(height, categories.underweight.name),
+    normal: weightRangeKg(height, categories.normal.name),
+    overweight: weightRangeKg(height, categories.overweight.name),
+    obese: weightRangeKg(height, categories.obese.name),
+  };
+};
+
+/**
+ *
+ * @param {Number} height in
+ */
+const lbRange = (height) => {
+  return {
+    underweight: weightRangeLb(height, categories.underweight.name),
+    normal: weightRangeLb(height, categories.normal.name),
+    overweight: weightRangeLb(height, categories.overweight.name),
+    obese: weightRangeLb(height, categories.obese.name),
+  };
+};
+
+/**
+ * Retrieve BMI category
+ * @param {Number} bmi BMI index value
+ * @returns associated BMI category object
+ */
+const bmiCategory = (bmi) => {
+  const { underweight, normal, overweight, obese } = categories;
+  if (bmi < thresholds.underweight) return underweight;
+  else if (bmi < thresholds.normal) return normal;
+  else if (bmi < thresholds.overweight) return overweight;
+  else return obese;
+};
+
+/**
+ * Calculates a person's BMI based on height and weight
+ * @param {Number} kg weight in kilograms
+ * @param {Number} meters height in meters
+ * @returns
+ */
+const calculateBmi = (kg, meters) => kg / (meters * meters);
+
+export const BmiUtils = {
+  calculateBmi,
+  bmiCategory,
+  kgRange,
+  lbRange,
 };
