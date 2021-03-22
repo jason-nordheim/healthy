05e68a2dafef3cb03856fe6541e06e63f39e581f
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-22 08:31:30 -0600

    added component to render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BmiCalculator } from "./components/BmiCalculator";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <BmiCalculator />
   </React.StrictMode>,
   document.getElementById("root")
 );
