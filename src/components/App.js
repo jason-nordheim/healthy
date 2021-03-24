5f0d59d8e7c1abc9540ac5b811218b51462b2879
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 15:26:30 -0600

    wrapped all components in AuthContext.Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
+import { useReducer } from "react";
 import { Route } from "react-router";
+import { AuthContext, AuthReducer } from "../context/auth.context";
 import { AppRoutes } from "../routes";
 import { NavBar } from "./layout/nav/NavBar";
 
 export const App = () => {
+  const [state, dispatch] = useReducer(AuthReducer);
   return (
-    <>
+    <AuthContext.Provider value={[state, dispatch]}>
       <NavBar />
       {AppRoutes.map((route) => {
         const { path, exact, component } = route;
@@ -12,6 +15,6 @@ export const App = () => {
           <Route key={path} exact={exact} path={path} render={component} />
         );
       })}
-    </>
+    </AuthContext.Provider>
   );
 };
