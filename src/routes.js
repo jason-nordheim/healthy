ac7b3850f34ac62a898b562647ffc412f8dcd49d
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 13:12:58 -0600

    added '/account' route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { HomePage } from "./components/pages/HomePage";
 import { BmiCalculator } from "./components/BmiCalculator";
+import { AccountPage } from "./components/pages/AccountPage";
 
 export const AppRoutes = [
   { path: "/", exact: true, label: "Home", component: () => <HomePage /> },
+  {
+    path: "/account",
+    exact: false,
+    label: "Account",
+    component: () => <AccountPage />,
+  },
   {
     path: "/bmiCalculator",
     exact: false,
