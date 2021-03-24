0f0ec443cc1fa9e62b498d747bbeffd359f7d446
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 14:41:22 -0600

    added jumbotron

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,12 +3,13 @@ import { AppName } from "../../config";
 export const HomePage = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1 className="display-2">
-            Welcome to <u>{AppName}</u>!
-          </h1>
-        </div>
+      <div className="jumbotron">
+        <h1 className="display-2 mt-3">{AppName}!</h1>
+        <hr class="my-4" />
+        <p class="lead">
+          Welcome to {AppName}! Let us help you live your bStart on your journey to your
+          best self.
+        </p>
       </div>
     </div>
   );
