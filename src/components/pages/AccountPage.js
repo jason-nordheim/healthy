4727f17e277375eb60b7755a7132e74877d42b4c
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 14:21:45 -0600

    added ability to toggle <LoginForm /> and <RegistrationForm />

--- src/components/pages/AccountPage.js
+++ src/components/pages/AccountPage.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
 import { RegisterForm } from "../forms/RegisterForm";
+import { LoginForm } from "../forms/LoginForm";
 
 export const AccountPage = () => {
+  const [showRegistration, setShowRegistration] = useState(false);
+  const toggleForms = (e) => {
+    setShowRegistration(!showRegistration);
+  };
   return (
     <div className="container">
-      <RegisterForm />
+      {showRegistration ? (
+        <>
+          <div className="row">
+            <div className="col mt-3">
+              <RegisterForm />
+            </div>
+          </div>
+          <div className="row">
+            <span>
+              <h6>
+                Don't have an account? Register{" "}
+                <u style={{ cursor: "pointer" }} onClick={toggleForms}>
+                  here
+                </u>
+              </h6>
+            </span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="row">
+            <div className="col mt-3">
+              <LoginForm />
+            </div>
+          </div>
+          <div className="row">
+            <span className="col mb-3 mt-3    ">
+              <h6 className="text-center">
+                Already have an account? Login{" "}
+                <u style={{ cursor: "pointer" }} onClick={toggleForms}>
+                  here
+                </u>
+              </h6>
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
