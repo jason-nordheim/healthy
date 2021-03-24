45f53e2ed2cd7e934b6eb32897d4f7c07864534e
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 13:14:32 -0600

    created config file for global constants

--- src/config.js
+++ src/config.js
@@ -1,4 +1,16 @@
 export const AppName = "Healthy";
 
-
-
+export const MONTHS = [
+  { id: 1, name: "January", value: "JAN" },
+  { id: 2, name: "February", value: "FEB" },
+  { id: 3, name: "March", value: "MAR" },
+  { id: 4, name: "April", value: "APR" },
+  { id: 5, name: "May", value: "MAY" },
+  { id: 6, name: "June", value: "JUN" },
+  { id: 7, name: "July", value: "JUL" },
+  { id: 8, name: "August", value: "AUG" },
+  { id: 9, name: "September", value: "SEP" },
+  { id: 10, name: "October", value: "OCT" },
+  { id: 11, name: "November", value: "NOV" },
+  { id: 12, name: "December", value: "DEC" },
+];
