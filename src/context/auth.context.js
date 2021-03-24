cc852948eef25e6d1f2697d1fef8d7ab5045bc14
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-24 15:53:03 -0600

    renamed ACTIONS to AUTH_TYPE

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -10,7 +10,10 @@ export const AuthContext = createContext({
   error: undefined,
 });
 
-const ACTIONS = {
+/***
+ * Types of Action
+ */
+const AUTH_TYPE = {
   // login Actions
   LOGIN_REQUEST: "LOGIN_REQUEST",
   LOGIN_SUCCESS: "LOGIN_SUCCESS",
@@ -27,19 +30,19 @@ const ACTIONS = {
 
 const Login = (user, dispatch) => {
   // request started
-  dispatch({ type: ACTIONS.LOGIN_REQUEST, payload: null });
+  dispatch({ type: AUTH_TYPE.LOGIN_REQUEST, payload: null });
   return loginUser(user)
     .then((res) => res.json())
     .then((data) => {
       if (data.error) {
         dispatch({
-          type: ACTIONS.LOGIN_FAILURE,
+          type: AUTH_TYPE.LOGIN_FAILURE,
           payload: { error: data.error },
         });
       }
       console.log("data", data);
       dispatch({
-        type: ACTIONS.LOGIN_SUCCESS,
+        type: AUTH_TYPE.LOGIN_SUCCESS,
         payload: { token: data.token },
       });
     })
@@ -47,7 +50,7 @@ const Login = (user, dispatch) => {
       // todo: have not found scenario in which this is invoked
       console.error("LoginError", error);
       dispatch({
-        type: ACTIONS.LOGIN_FAILURE,
+        type: AUTH_TYPE.LOGIN_FAILURE,
         payload: { error },
       });
     });
@@ -55,27 +58,27 @@ const Login = (user, dispatch) => {
 
 const Logout = (dispatch) => {
   try {
-    dispatch({ type: ACTIONS.LOGOUT_REQUEST });
-    dispatch({ type: ACTIONS.LOGOUT_SUCCESS });
+    dispatch({ type: AUTH_TYPE.LOGOUT_REQUEST });
+    dispatch({ type: AUTH_TYPE.LOGOUT_SUCCESS });
   } catch (error) {
     // todo: this is never triggered currently
-    dispatch({ type: ACTIONS.LOGIN_FAILURE });
+    dispatch({ type: AUTH_TYPE.LOGIN_FAILURE });
   }
 };
 
 const Register = (user, dispatch) => {
-  dispatch({ type: ACTIONS.REGISTER_REQUEST });
+  dispatch({ type: AUTH_TYPE.REGISTER_REQUEST });
 
   registerUser(user)
     .then((res) => res.json())
     .then((data) => {
       if (data.error) {
         dispatch({
-          type: ACTIONS.REGISTER_FAILURE,
+          type: AUTH_TYPE.REGISTER_FAILURE,
           payload: { error: data.error },
         });
       } else {
-        dispatch({ type: ACTIONS.REGISTER_SUCCESS, payload: { data } });
+        dispatch({ type: AUTH_TYPE.REGISTER_SUCCESS, payload: { data } });
 
         // start the login process
         Login(user, dispatch);
@@ -85,7 +88,7 @@ const Register = (user, dispatch) => {
       // todo: this needs to be tested
       console.error(error);
       return dispatch({
-        type: ACTIONS.REGISTER_FAILURE,
+        type: AUTH_TYPE.REGISTER_FAILURE,
         payload: { error },
       });
     });
@@ -99,56 +102,55 @@ export const AuthActions = {
 
 export const AuthReducer = (state, action) => {
   switch (action.type) {
-    case ACTIONS.LOGIN_REQUEST:
+    case AUTH_TYPE.LOGIN_REQUEST:
       return {
         ...state,
         request_in_progress: true,
         token: undefined,
         error: undefined,
       };
-    case ACTIONS.LOGIN_SUCCESS:
+    case AUTH_TYPE.LOGIN_SUCCESS:
       return {
         ...state,
         request_in_progress: false,
         token: action.payload.token,
       };
-    case ACTIONS.LOGIN_FAILURE:
+    case AUTH_TYPE.LOGIN_FAILURE:
       return {
         ...state,
         request_in_progress: false,
         error: action.payload.error,
       };
-    case ACTIONS.LOGOUT_REQUEST:
+    case AUTH_TYPE.LOGOUT_REQUEST:
       return {
         ...state,
         request_in_progress: true,
         token: undefined,
         error: undefined,
       };
-    case ACTIONS.LOGOUT_SUCCESS:
+    case AUTH_TYPE.LOGOUT_SUCCESS:
       return {
         ...state,
         request_in_progress: false,
         error: undefined,
       };
-    case ACTIONS.LOGOUT_FAILURE:
+    case AUTH_TYPE.LOGOUT_FAILURE:
       return {
         ...state,
         request_in_progress: false,
         error: action.payload.error,
       };
-    case ACTIONS.REGISTER_REQUEST:
+    case AUTH_TYPE.REGISTER_REQUEST:
       return {
         ...state,
         request_in_progress: true,
       };
-      break;
-    case ACTIONS.REGISTER_SUCCESS:
+    case AUTH_TYPE.REGISTER_SUCCESS:
       return {
         ...state,
         request_in_progress: false,
       };
-    case ACTIONS.REGISTER_FAILURE:
+    case AUTH_TYPE.REGISTER_FAILURE:
       return {
         ...state,
         request_in_progress: false,
