2b1e9256a9458263eb1963f119055d04fec73139
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-25 12:37:35 -0600

    removed context as it was not needed

--- src/components/pages/ToolsPage.js
+++ src/components/pages/ToolsPage.js
@@ -1,10 +1,6 @@
-import { useContext } from "react";
-import { AuthContext } from "../../context/auth.context";
 import { BmiCalculator } from "../feature/BmiCalculator";
 
 export const ToolsPage = () => {
-  const [state, dispatch] = useContext(AuthContext);
-
   return (
     <div className="container">
       <div className="row">
