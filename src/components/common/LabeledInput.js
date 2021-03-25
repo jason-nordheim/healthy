1fef9f75476e9adb9adccb96c7316dc932f46be8
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-25 11:41:45 -0600

    updated <LabelInput /> to better work for number input types

--- src/components/common/LabeledInput.js
+++ src/components/common/LabeledInput.js
@@ -9,8 +9,27 @@ export const LabeledInput = ({
   onChange,
   type = "text",
   required = false,
+  min = 1,
+  max = Number.MAX_SAFE_INTEGER,
 }) => {
-  return (
+  return type === "number" ? (
+    <>
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      <input
+        type={type}
+        className="form-control"
+        id={id}
+        value={value}
+        name={name}
+        min={min}
+        max={max}
+        required={required}
+        onChange={onChange}
+      />
+    </>
+  ) : (
     <>
       <label htmlFor={name} className="form-label">
         {label}
