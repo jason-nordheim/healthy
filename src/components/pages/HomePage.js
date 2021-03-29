dda57fa21acec8acd25fe904833eba60cffbb635
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-29 13:54:58 -0600

    modified the homepage text

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,8 +5,8 @@ export const HomePage = () => {
     <div className="container">
       <div className="jumbotron">
         <h1 className="display-2 mt-3">{AppName}!</h1>
-        <hr class="my-4" />
-        <p class="lead">
+        <hr className="my-4" />
+        <p className="lead">
           Welcome to {AppName}! Let us help you live your bStart on your journey
           to your best self.
         </p>
