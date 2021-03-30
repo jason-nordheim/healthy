dd5a3653ad97ae5f256711f2eae7cd6733c0a765
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 12:30:27 -0600

    refactored to use new components

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -1,8 +1,14 @@
 import { useState, useContext } from "react";
-import { DEFAULTS, MONTHS } from "../../config";
+import { CLASSES, DEFAULTS } from "../../config";
 import { AuthActions, AuthContext } from "../../context/auth.context";
-import { LabeledInput } from "../common/LabeledInput";
-import { LabeledSelect } from "../common/LabeledSelect";
+import { FormTitle } from "./FormTitle";
+import { TextInput } from "./input/TextInput";
+import { SubmitButton } from "./input/SubmitButton";
+
+import { Label } from "./Label";
+import { PasswordInput } from "./input/PasswordInput";
+import { SelectBirthday } from "./select/SelectBirthday";
+import { FormErrorMessage } from "./FormErrorMessage";
 
 export const RegisterForm = () => {
   // context
@@ -14,11 +20,11 @@ export const RegisterForm = () => {
 
   // de-structure for readability
   const { first, last, email, password } = formValues;
-  const { day, month, year } = birthday;
 
   // submit event handler
   const handleSubmit = (event) => {
     event.preventDefault();
+    const { day, month, year } = birthday;
     const user = {
       first,
       last,
@@ -31,142 +37,85 @@ export const RegisterForm = () => {
     AuthActions.Register(user, dispatch);
   };
 
-  // birthday onChange event handler
-  const handleBirthdayChange = (event) => {
-    const { name, value } = event.target;
-    setBirthday({ ...birthday, [name]: value });
-  };
   // form onChange event handler
   const handleFormValueChange = (event) => {
     const { name, value } = event.target;
     setFormValues({ ...formValues, [name]: value });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col mt-3 mb-3">
-          <h2 className="text-center">Register</h2>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col mb-3">
-          <label htmlFor="first" className="form-label">
-            First Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="fist"
-            value={first}
-            name="first"
-            required
-            onChange={handleFormValueChange}
-          />
-        </div>
-        <div className="col mb-3">
-          <LabeledInput
-            for="last"
-            type="text"
-            name="last"
-            label="Last Name"
-            id="last"
-            value={last}
-            onChange={handleFormValueChange}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <label htmlFor="birthday" className="form-label">
-          Birthday
-        </label>
-        <div className="col mb-3">
-          <LabeledInput
-            id="day"
-            name="day"
-            label="Day"
-            value={day}
-            onChange={handleBirthdayChange}
-            type="number"
-            min={1}
-            max={31}
-          />
-        </div>
-        <div className="col mb-3">
-          <LabeledSelect
-            id="month"
-            name="month"
-            label="Month"
-            value={month}
-            required={true}
-            onChange={handleBirthdayChange}
-            selectOptions={MONTHS.map((m) => {
-              return {
-                key: m.value,
-                id: m.id,
-                value: m.name,
-              };
-            })}
-          />
-        </div>
-        <div className="col mb-3">
-          <LabeledInput
-            for="year"
-            id="year"
-            label="Year"
-            type="number"
-            name="year"
-            value={year}
-            required={true}
-            onChange={handleBirthdayChange}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col mb-3">
-          <LabeledInput
-            for="email"
-            id="email"
-            label="Email Address"
-            name="email"
-            value={email}
-            required={true}
-            onChange={handleFormValueChange}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="mb-3">
-          <LabeledInput
-            for="password"
-            type="password"
-            id="password"
-            label="Password"
-            name="password"
-            value={password}
-            required={true}
-            onChange={handleFormValueChange}
-          />
-          <div id="passwordHelp " className="form-text">
-            Your password must be 8-20 characters long, contain letters and
-            numbers, and must not contain spaces, special characters, or emoji.
+    <form onSubmit={handleSubmit} className={CLASSES.DEFAULT.FORMS}>
+      <FormTitle title="Register" />
+
+      <div className="container">
+        <div className="row">
+          <div className="col mb-3">
+            <Label label="First Name" name="fist" />
+            <TextInput
+              type="text"
+              className="form-control"
+              id="fist"
+              value={first}
+              name="first"
+              required
+              onChange={handleFormValueChange}
+            />
+          </div>
+          <div className="col mb-3">
+            <Label label="Last Name" name="fist" />
+            <TextInput
+              for="last"
+              type="text"
+              name="last"
+              label="Last Name"
+              id="last"
+              value={last}
+              onChange={handleFormValueChange}
+            />
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={handleSubmit}
-          >
-            Register
-          </button>
+
+      <SelectBirthday birthday={birthday} setBirthday={setBirthday} />
+
+      <div className="container">
+        <div className="row">
+          <div className="col mb-3">
+            <Label label="Email Address" name="fist" />
+            <TextInput
+              for="email"
+              id="email"
+              label="Email Address"
+              name="email"
+              value={email}
+              required={true}
+              onChange={handleFormValueChange}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col mb-3">
+            <Label label="Password" name="password" />
+            <PasswordInput
+              password={password}
+              onChange={handleFormValueChange}
+            />
+          </div>
         </div>
       </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col position-relative end">
+            <SubmitButton onSubmit={handleSubmit} label="Login" />
+          </div>
+        </div>
+      </div>
+
       {state?.error && (
         <div className="row">
           <div className="col mt-3">
-            <h6 className="text-danger text-center">{state.error}</h6>
+            <FormErrorMessage message={state.error} />
           </div>
         </div>
       )}
