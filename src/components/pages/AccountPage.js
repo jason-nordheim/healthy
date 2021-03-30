2e69ec103590750ac8d50de4dbba645e2178c446
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 12:28:12 -0600

    refactored for readability and to use <PageTItle /> compoent

--- src/components/pages/AccountPage.js
+++ src/components/pages/AccountPage.js
@@ -3,6 +3,7 @@ import { RegisterForm } from "../forms/RegisterForm";
 import { LoginForm } from "../forms/LoginForm";
 import { AuthActions, AuthContext } from "../../context/auth.context";
 import { EditProfile } from "../forms/EditProfile";
+import { PageTitle } from "../layout/PageTitle";
 
 /**
  * Account Page for Unauthenticated users
@@ -11,45 +12,43 @@ const UnAuthenticated = () => {
   const [showRegistration, setShowRegistration] = useState(false);
   const toggleForms = (e) => setShowRegistration(!showRegistration);
 
+  const DisplayedForm = () =>
+    showRegistration ? <RegisterForm /> : <LoginForm />;
+
+  const SwitchForms = () =>
+    showRegistration ? (
+      <h6 className="text-center">
+        Don't have an account? Register{" "}
+        <u style={{ cursor: "pointer" }} onClick={toggleForms}>
+          here
+        </u>
+      </h6>
+    ) : (
+      <h6 className="text-center">
+        Already have an account? Login{" "}
+        <u style={{ cursor: "pointer" }} onClick={toggleForms}>
+          here
+        </u>
+      </h6>
+    );
+
   return (
     <div className="container">
-      {showRegistration ? (
-        <>
-          <div className="row">
-            <div className="col mt-3">
-              <RegisterForm />
-            </div>
-          </div>
-          <div className="row">
-            <span className="col mb-3 mt-3">
-              <h6 className="text-center">
-                Don't have an account? Register{" "}
-                <u style={{ cursor: "pointer" }} onClick={toggleForms}>
-                  here
-                </u>
-              </h6>
-            </span>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="row">
-            <div className="col mt-3">
-              <LoginForm />
-            </div>
-          </div>
-          <div className="row">
-            <span className="col mb-3 mt-3">
-              <h6 className="text-center">
-                Already have an account? Login{" "}
-                <u style={{ cursor: "pointer" }} onClick={toggleForms}>
-                  here
-                </u>
-              </h6>
-            </span>
-          </div>
-        </>
-      )}
+      <div className="row">
+        <div className="col">
+          <PageTitle text={`My Account`} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col mt-3">
+          <DisplayedForm />
+        </div>
+      </div>
+      <div className="row">
+        <span className="col mb-3 mt-3">
+          <SwitchForms />
+        </span>
+      </div>
     </div>
   );
 };
@@ -65,11 +64,8 @@ const Authenticated = ({ state, dispatch }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="jumbotron mb-3 mt-3">
-          <h1>Welcome</h1>
-        </div>
+        <PageTitle text="Welcome" />
       </div>
-      <hr className="my-4" />
       <div className="row">
         <div className="col">
           <p className="lead">
@@ -77,7 +73,9 @@ const Authenticated = ({ state, dispatch }) => {
           </p>
         </div>
       </div>
-      <EditProfile />
+      <div className="container">
+        <EditProfile />
+      </div>
       <div className="row">
         <div className="col">
           <button className="btn btn-primary" onClick={logout}>
@@ -91,9 +89,6 @@ const Authenticated = ({ state, dispatch }) => {
 
 export const AccountPage = () => {
   const [state, dispatch] = useContext(AuthContext);
-  return state?.token ? (
-    <Authenticated state={state} dispatch={dispatch} />
-  ) : (
-    <UnAuthenticated />
-  );
+  if (state?.token) return <Authenticated state={state} dispatch={dispatch} />;
+  else return <UnAuthenticated />;
 };
