4103eeda643ae79b0153fcdb48dda483206e7f9a
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 12:27:55 -0600

    refactored to use <PageTitle /> component

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,16 +1,14 @@
 import { AppName } from "../../config";
+import { PageTitle } from "../layout/PageTitle";
 
 export const HomePage = () => {
   return (
     <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-2 mt-3">{AppName}!</h1>
-        <hr className="my-4" />
-        <p className="lead">
-          Welcome to {AppName}! Let us help you live your bStart on your journey
-          to your best self.
-        </p>
-      </div>
+      <PageTitle text={AppName} />
+      <p className="lead">
+        Welcome to {AppName}! Let us help you live your bStart on your journey
+        to your best self.
+      </p>
     </div>
   );
 };
