762a68eff516be76513215cb70d6777ad697d109
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 12:27:38 -0600

    refactored to use <PageTitle /> component

--- src/components/pages/ToolsPage.js
+++ src/components/pages/ToolsPage.js
@@ -1,8 +1,14 @@
+import { PageTitle } from "../layout/PageTitle";
 import { BmiCalculator } from "../feature/BmiCalculator";
 
 export const ToolsPage = () => {
   return (
     <div className="container">
+      <div className="row">
+        <div className="col">
+          <PageTitle text="Healthy Tools" />
+        </div>
+      </div>
       <div className="row">
         <div className="col">
           <BmiCalculator />
