0b183cb0dfa7bbd5535269619826c1d043b1236c
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 12:29:02 -0600

    created re-usable <FormTitle /> component to standardize the way that form title are presented

---
+++ src/components/forms/FormTitle.js
@@ -0,0 +1,11 @@
+export const FormTitle = ({ title }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col mt-4 mb-3">
+          <h1>{title}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
