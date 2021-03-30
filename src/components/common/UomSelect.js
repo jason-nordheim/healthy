159b6f5e27329e2ec2081a2742d5a8e51fe4ce8c
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 10:47:18 -0600

    created <SelectUnits /> component to encapsulate logic related to selecting units of measure

---
+++ src/components/common/UomSelect.js
@@ -0,0 +1,32 @@
+import { UOM } from "../../config";
+
+export const SelectUnits = ({ uom, onChangeUom }) => {
+  return (
+    <span className="input-group p-3">
+      <label className="input-group-text" htmlFor="uom">
+        Unit of Measure
+      </label>
+      <select
+        className="form-select"
+        aria-label="Unit of Measurement"
+        name="uom"
+        id="uom"
+        value={uom}
+        onChange={onChangeUom}
+      >
+        <option
+          value={UOM.IMPERIAL}
+          className={uom === UOM.IMPERIAL ? "selected" : ""}
+        >
+          {UOM.IMPERIAL}
+        </option>
+        <option
+          value={UOM.METRIC}
+          className={uom === UOM.METRIC ? "selected" : ""}
+        >
+          {UOM.METRIC}
+        </option>
+      </select>
+    </span>
+  );
+};
