78bd3fb83f496d8fb297df28fb1fe107c9487c99
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-30 14:04:24 -0600

    seperated out into functions with comments

--- src/util/UnitUtilities.js
+++ src/util/UnitUtilities.js
@@ -1,8 +1,36 @@
+/**
+ * returns the equivalent number of inches
+ * @param {Number} inches
+ * @returns
+ */
+const inchesToCentimeters = (inches) => inches * 2.54;
+
+/**
+ * returns the equivalent number of inches
+ * @param {Number} centimeters
+ * @returns {Number} inches
+ */
+const centimetersToInches = (centimeters) => centimeters / 2.54;
+
+/**
+ * return the equivalent number of kilograms
+ * @param {Number} pounds
+ * @returns {Number} kilograms
+ */
+const poundsToKilograms = (pounds) => pounds / 2.205;
+
+/**
+ * returns the equivalent number of pounds
+ * @param {Number} kilograms
+ * @returns {Number} pounds
+ */
+const kilogramsToPounds = (kilograms) => kilograms * 2.205;
+
 export const convert = {
-  inchesToCentimeters: (inches) => inches * 2.54,
-  centimetersToInches: (centimeters) => centimeters / 2.54,
-  poundsToKilograms: (pounds) => pounds / 2.205,
-  kilogramsToPounds: (kilograms) => kilograms * 2.205,
+  inchesToCentimeters,
+  centimetersToInches,
+  poundsToKilograms,
+  kilogramsToPounds,
 };
 
 export const round = (number, decimals = 2) => {
