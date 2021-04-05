dd756c6567d443581f0bcba0aaef3e8a1e299d9c
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-05 11:29:27 -0600

    passed initial field values via props

--- src/components/forms/EditProfile.js
+++ src/components/forms/EditProfile.js
@@ -1,44 +1,26 @@
 import { useContext, useEffect, useState } from "react";
-import { CLASSES, DEFAULTS, UOM } from "../../config";
-import { AuthActions, AuthContext } from "../../context/auth.context";
+import { CLASSES, UOM } from "../../config";
+import { AuthContext } from "../../context/auth.context";
 import { SelectBirthday } from "./select/SelectBirthday";
 import { SelectHeight } from "./select/SelectHeight";
 import { SelectUnits } from "./select/SelectUnits";
 import { FormTitle } from "./FormTitle";
 import { TextInput } from "./input/TextInput";
 import { Label } from "./Label";
-import { getProfile } from "../../util/ApiUtils";
-import { TokenExpiredError } from "../../errors/TokenExpiredError";
 
-export const EditProfile = () => {
-  const [state, dispatch] = useContext(AuthContext);
-  const [fields, setFields] = useState(DEFAULTS.USER);
-  const [birthday, setBirthday] = useState(DEFAULTS.BIRTHDAY);
-  const [centimeters, setCentimeters] = useState(DEFAULTS.MEASUREMENTS.HEIGHT);
+export const EditProfile = ({ userData }) => {
+  const [fields, setFields] = useState({
+    first: userData.first,
+    last: userData.last,
+    email: userData.email,
+  });
+  const [birthday, setBirthday] = useState({
+    day: userData.day,
+    month: userData.month,
+    year: userData.year,
+  });
+  const [centimeters, setCentimeters] = useState(userData.height || 0);
   const [uom, setUom] = useState(UOM.IMPERIAL);
-
-  // retrieve user profile when the component mounts
-  useEffect(() => {
-    state?.token &&
-      getProfile(state.token)
-        .then((res) => {
-          if (res.status === 403) throw new TokenExpiredError();
-          return res.json();
-        })
-        .then((data) => {
-          const { first, last, email, day, month, year } = data;
-
-          console.log({ data, b: DEFAULTS.BIRTHDAY });
-          setFields({ ...fields, first, last, email });
-          setBirthday({ day, month, year });
-        })
-        .catch((error) => {
-          console.log("inCatch");
-          if (typeof error === typeof new TokenExpiredError()) {
-            AuthActions.Logout(dispatch);
-          }
-        });
-  }, [state.token]);
 
   // event handler for changing the UOM
   const handleUomChange = (event) => {
