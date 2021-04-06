bc0eecd68fd3a95c452c3fb6d05fba77b4511234
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-06 10:23:28 -0600

    created user context

--- src/context/auth.actions.js
+++ src/context/auth.actions.js
@@ -65,6 +65,7 @@ const Register = (user, dispatch) => {
     });
 };
 
+
 export const AuthActions = {
   Login,
   Logout,
---
+++ src/context/user.type.js
@@ -0,0 +1,5 @@
+export const USER_TYPE = {
+  PROFILE_REQUEST: "PROFILE_REQUEST",
+  PROFILE_SUCCESS: "PROFILE_SUCCESS",
+  PROFILE_FAILURE: "PROFILE_FAILURE", // failed to retrieve profile information
+};
