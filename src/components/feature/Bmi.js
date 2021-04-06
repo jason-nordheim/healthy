5bbc62b6c89cde7f30062dd35aac8bbaaaef76f4
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-06 16:04:27 -0600

    simplified to just text

--- src/components/feature/Bmi.js
+++ src/components/feature/Bmi.js
@@ -1,10 +1,8 @@
-import { categories, BmiUtils } from "../../util/BmiUtils";
+import { categories } from "../../util/BmiUtils";
 import { round } from "../../util/UnitUtilities";
 import { Label } from "../forms/Label";
 
-const { bmiCategory } = BmiUtils;
-export const Bmi = ({ bmi }) => {
-  const category = bmiCategory(bmi);
+export const Bmi = ({ category, bmi }) => {
   return (
     <>
       <div className="row">
@@ -12,36 +10,21 @@ export const Bmi = ({ bmi }) => {
       </div>
       <div className="row mt-3 mb-3">
         <div className="col-sm-auto">
-          <div
-            className="progress"
-            style={{ height: "1.5rem", textJustify: "center" }}
+          <span
+            className={
+              category?.name === categories.underweight.name
+                ? "text-warning"
+                : category?.name === categories.normal.name
+                ? "text-success"
+                : category?.name === categories.overweight.name
+                ? "text-warning"
+                : category?.name === categories.obese.name
+                ? "text-danger"
+                : ""
+            }
           >
-            <div
-              role="progressbar"
-              style={{
-                width: `${category.percent * 100}%`,
-                height: "100%",
-                fontSize: "1.1rem",
-                textAlign: "center",
-              }}
-              className={
-                "progress-bar  " + category.name === categories.underweight.name
-                  ? "bg-warning"
-                  : category.name === categories.normal.name
-                  ? "bg-success"
-                  : category.name === categories.overweight.name
-                  ? "bg-warning"
-                  : category.name === categories.obese.name
-                  ? "bg-danger"
-                  : ""
-              }
-              aria-valuenow={bmi}
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >
-              {round(bmi)}
-            </div>
-          </div>
+            {round(bmi)}
+          </span>
         </div>
       </div>
     </>
