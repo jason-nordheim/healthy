883e98237aec774ab3330f9949489286b38587a0
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-06 10:25:19 -0600

    added 'auto' for responsive resizing

--- src/components/forms/FormTitle.js
+++ src/components/forms/FormTitle.js
@@ -2,7 +2,7 @@ export const FormTitle = ({ title }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col mt-4 mb-3">
+        <div className="col-sm-auto mt-4 mb-3">
           <h1>{title}</h1>
         </div>
       </div>
