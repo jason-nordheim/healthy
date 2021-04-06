82c721602aa2c743ca714fd5b0c9cb81c178554f
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-06 16:36:53 -0600

    added menu and the ability to select which calculator you want to use

--- src/components/pages/ToolsPage.js
+++ src/components/pages/ToolsPage.js
@@ -1,6 +1,6 @@
 import { PageTitle } from "../layout/PageTitle";
 import { BmiCalculator } from "../feature/BmiCalculator";
-import { useContext, useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { UserActions } from "../../context/user.actions";
 import { UserReducer } from "../../context/user.reducer";
 import { AuthContext } from "../../context/auth.context";
@@ -9,6 +9,8 @@ import { UserState } from "../../context/user.state";
 export const ToolsPage = () => {
   const [authState, authDispatch] = useContext(AuthContext);
   const [userState, userDispatch] = useReducer(UserReducer, UserState.initial);
+  const [expandMenu, setExpandMenu] = useState(false);
+  const [showCalc, setShowCalc] = useState(false);
 
   useEffect(() => {
     if (!authState) return;
@@ -24,7 +26,42 @@ export const ToolsPage = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col">
+        <div className="col-sm-auto">
+          <div className="container">
+            <div className="button-group">
+              <button
+                id="calculatorsDropdown"
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded={expandMenu}
+                onClick={(e) => setExpandMenu(!expandMenu)}
+              >
+                Calculators
+              </button>
+            </div>
+            <div
+              className={expandMenu ? "dropdown-menu show" : "dropdown-menu"}
+              aria-labelledby="calculatorsDropdown"
+              style={{ position: "relative" }}
+              onClick={(e) => setExpandMenu(!expandMenu)}
+            >
+              <button
+                className="btn dropdown-item"
+                type="button"
+                data-toggle={showCalc ? "collapse show" : "collapse"}
+                data-target="#bmiCalc"
+                aria-expanded={showCalc}
+                aria-controls="bmiCalc"
+                onClick={(e) => setShowCalc(!showCalc)}
+              >
+                BMI Calculator
+              </button>
+            </div>
+          </div>
+        </div>
+        <div id="bmiCalc" className={showCalc ? "collapse show" : "collapse"}>
           <BmiCalculator profile={userState.profile} />
         </div>
       </div>
