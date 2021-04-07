68d6edbe8932c148bcd427bfbb8c1cc22ae4c361
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-07 10:14:40 -0600

    removed exclamations mark

--- src/components/layout/PageTitle.js
+++ src/components/layout/PageTitle.js
@@ -5,7 +5,7 @@ export const PageTitle = ({ text, addOnClasses = [] }) => {
   }
   return (
     <div className={classes.join(" ")}>
-      <h1 className="display-2 mt-3">{text}!</h1>
+      <h1 className="display-2 mt-3">{text}</h1>
       <hr className="my-4" />
     </div>
   );
