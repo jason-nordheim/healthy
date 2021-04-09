d8c6292df7523afb7d49d76d394ee97f0a07e90f
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-09 10:36:18 -0600

    started panning activity log component

--- src/components/AcitivityLogContainer.js
+++ src/components/AcitivityLogContainer.js
@@ -1,9 +1,49 @@
+const MOCK_ACTIVITIES = [
+  { type: "Running", variation: "Outdoors", category: "Cardio" },
+  { type: "Running", variation: "Trail", category: "Cardio" },
+  { type: "Running", variation: "Treadmill", category: "Cardio" },
+  { type: "Walking", variation: "Outdoors", category: "Cardio" },
+  { type: "Walking", variation: "Trail", category: "Cardio" },
+  { type: "Walking", variation: "Treadmill", category: "Cardio" },
+  { type: "Swimming", variation: "Open Water", category: "Cardio" },
+  { type: "Swimming", variation: "Pool", category: "Cardio" },
+  { type: "Stair Climber", variation: "Machine", category: "Cardio" },
+  { type: "Rowing", variation: "Indoor", category: "Cardio" },
+  { type: "Rowing", variation: "Outdoor", category: "Cardio" },
+  { type: "Cycling", variation: "Indoor", category: "Cardio" },
+  { type: "Cycling", variation: "Outdoor", category: "Cardio" },
+];
+
 export const ActivityLogContainer = () => {
   return (
-    <div className="row">
-      <div className="col-sm-auto">
-        <h3>Activity Log</h3>
+    <>
+      <div className="row">
+        <div className="col-sm-auto">
+          <h3>Activity Log</h3>
+        </div>
       </div>
-    </div>
+      <div className="row">
+        <div className="col-sm-auto">
+          <ul className="list-group">
+            {MOCK_ACTIVITIES.map((activity) => {
+              return (
+                <li
+                  key={activity.type + activity.variation}
+                  className="list-group-item"
+                >
+                  <span>{activity.type}</span>{" "}
+                  <span
+                    className="text-secondary"
+                    style={{ fontStyle: "italic" }}
+                  >
+                    {activity.variation}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
