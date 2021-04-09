cc0b8f5bc836e039c0e9969768e988ba6f4efca6
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-09 13:22:21 -0600

    added throwing of tokens if needed

--- src/util/ApiUtils.js
+++ src/util/ApiUtils.js
@@ -1,8 +1,10 @@
 import { API_CONFIG } from "../config/api.config";
+import { TokenExpiredError } from "../errors/TokenExpiredError";
 
 export const loginUser = (user) => {
-  return fetch(API_CONFIG.routes.auth.url, {
-    method: API_CONFIG.routes.auth.method,
+  const { url, method } = API_CONFIG.routes.auth;
+  return fetch(url, {
+    method,
     headers: {
       "Content-Type": "application/json",
     },
@@ -11,43 +13,91 @@ export const loginUser = (user) => {
 };
 
 export const registerUser = (user) => {
-  return fetch(API_CONFIG.routes.user.register.url, {
-    method: API_CONFIG.routes.user.register.method,
+  const { url, method } = API_CONFIG.routes.user.register;
+  return fetch(url, {
+    method,
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ user }),
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    else return res.json();
   });
 };
 
 export const getProfile = (token) => {
-  return fetch(API_CONFIG.routes.user.getInfo.url, {
-    method: API_CONFIG.routes.user.getInfo.method,
+  const { url, method } = API_CONFIG.routes.user.getInfo;
+  return fetch(url, {
+    method,
     headers: {
       "Content-Type": "application/json",
       Authorization: `bearer ${token}`,
     },
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    else return res.json();
   });
 };
 
 export const updateProfile = (token, fields) => {
-  return fetch(API_CONFIG.routes.user.updateInfo.url, {
-    method: API_CONFIG.routes.user.updateInfo.method,
+  const { url, method } = API_CONFIG.routes.user.updateInfo;
+  return fetch(url, {
+    method,
     headers: {
       "Content-Type": "application/json",
       Authorization: `bearer ${token}`,
     },
     body: JSON.stringify(fields),
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    else return res.json();
   });
 };
 
 export const addWeight = (token, kg) => {
-  return fetch(API_CONFIG.routes.weight.add.url, {
-    method: API_CONFIG.routes.weight.add.method,
+  const { url, method } = API_CONFIG.routes.weight.add;
+  return fetch(url, {
+    method,
     headers: {
       "Content-Type": "application/json",
       Authorization: `bearer ${token}`,
     },
     body: JSON.stringify({ kg, source: "web" }),
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    // convert to json
+    else return res.json();
+  });
+};
+
+export const getWeight = (token) => {
+  const { url, method } = API_CONFIG.routes.weight.getAll;
+  return fetch(url, {
+    method,
+    headers: {
+      Authorization: `bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    // convert the body to json
+    else return res.json();
+  });
+};
+
+export const deleteWeight = (token, weightId) => {
+  const { url, method } = API_CONFIG.routes.weight.deleteOne;
+  return fetch(url, {
+    method,
+    headers: {
+      Authorization: `bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ _id: weightId }),
+  }).then((res) => {
+    if (res.status === 403) throw new TokenExpiredError();
+    // no content to the response
+    else return res.text();
   });
 };
