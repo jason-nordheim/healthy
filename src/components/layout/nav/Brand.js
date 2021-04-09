8690ecbb7b787ea05df50ec1227a4a0ab60e73b0
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-09 14:54:14 -0600

    moved toggle outside of brand

--- src/components/layout/nav/Brand.js
+++ src/components/layout/nav/Brand.js
@@ -1,23 +1,11 @@
 import { AppName } from "../../../config/app.config";
 
-export const Brand = ({ expanded, setExpanded }) => {
+export const Brand = () => {
   return (
     <>
       <a className="navbar-brand" href="/">
         {AppName}
       </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle={expanded ? "expand" : "collapse"}
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded={expanded ? "true" : "false"}
-        aria-label="Toggle navigation"
-        onClick={() => setExpanded(!expanded)}
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
     </>
   );
 };
