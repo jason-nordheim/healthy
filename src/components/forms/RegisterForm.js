90620c31ff85f08e4022987dff2b0dec61ca7007
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-14 08:53:34 -0600

    changed classes for formatting

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -11,7 +11,6 @@ import { FormErrorMessage } from "./FormErrorMessage";
 import { AuthActions } from "../../context/auth.actions";
 
 export const RegisterForm = () => {
-  // context
   const [state, dispatch] = useContext(AuthContext);
 
   // component state
@@ -52,37 +51,42 @@ export const RegisterForm = () => {
     });
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      className="d-flex flex-column justify-content-center"
+      onSubmit={handleSubmit}
+    >
       <FormTitle title="Register" />
 
-      <div className="container mb-3">
+      <div className="container mb-3 ">
         <div className="row">
           <div className="col-sm-auto">
             <Label label="Name" />
           </div>
-          <div className="col-sm-auto mb-2">
-            <span className="input-group">
-              <Label label="First" name="first" inputText={true} />
-              <TextInput
-                for="first"
-                name="first"
-                id="first"
-                value={first}
-                onChange={handleFieldChange}
-              />
-            </span>
-          </div>
-          <div className="col-sm-auto mb-2">
-            <span className="input-group">
-              <Label label="Last" name="last" inputText={true} />
-              <TextInput
-                for="last"
-                name="last"
-                id="last"
-                value={last}
-                onChange={handleFieldChange}
-              />
-            </span>
+          <div className="row">
+            <div className="col-sm-auto mb-2">
+              <span className="input-group">
+                <Label label="First" name="first" inputText={true} />
+                <TextInput
+                  for="first"
+                  name="first"
+                  id="first"
+                  value={first}
+                  onChange={handleFieldChange}
+                />
+              </span>
+            </div>
+            <div className="col-sm-auto mb-2">
+              <span className="input-group">
+                <Label label="Last" name="last" inputText={true} />
+                <TextInput
+                  for="last"
+                  name="last"
+                  id="last"
+                  value={last}
+                  onChange={handleFieldChange}
+                />
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -92,10 +96,12 @@ export const RegisterForm = () => {
       <div className="container">
         <div className="row">
           <div className="col-sm-auto">
-            <Label label="Login" />
+            <Label label="Credentials" />
           </div>
-          <div className="col-sm-auto mb-1">
-            <span className="input-group">
+        </div>
+        <div className="row">
+          <div className="col-sm-auto">
+            <span className="input-group mb-2">
               <Label label="Email" name="fist" inputText={true} />
               <TextInput
                 for="email"
@@ -107,27 +113,28 @@ export const RegisterForm = () => {
                 onChange={handleFieldChange}
               />
             </span>
-          </div>
-          <div className="col-sm-auto">
-            <span className="input-group">
-              <Label label="Password" name="password" inputText={true} />
-              <PasswordInput password={password} onChange={handleFieldChange} />
-            </span>
+            <div className="col-sm-auto">
+              <span className="input-group">
+                <Label label="Password" name="password" inputText={true} />
+                <PasswordInput
+                  password={password}
+                  onChange={handleFieldChange}
+                />
+              </span>
+            </div>
           </div>
         </div>
 
-        <div className="row">
-          <div className="col mb-3"></div>
-        </div>
+        <div className="row mb-3"></div>
       </div>
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-auto ">
+          <div className="col-sm-auto d-flex justify-content-center">
             <SubmitButton
               onSubmit={handleSubmit}
-              label="Login"
-              className="position-relative end"
+              label="Create Account"
+              className=""
             />
           </div>
         </div>
