68a91a72b2121f90f503041bf3f1aa3cfc6451bf
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-14 17:15:45 -0600

    added button and changed classes for better visual heirarchy

--- src/components/feature/Food.js
+++ src/components/feature/Food.js
@@ -130,40 +130,45 @@ export const Food = ({ food }) => {
   const nutrientValues = Object.values(nutrients);
 
   return (
-    <>
-      <div className="row rounded">
-        <div className="col-sm-auto border my-2 ">
-          <span className="w-100">
-            <p className="pt-3 mx-auto">{label}</p>
-          </span>
-          <table className="table table-responsive table-bordered table-sm ">
-            <tbody className="table-striped">
-              <tr>
-                <td scope="row">Category</td>
-                <td className="fw-light">{category}</td>
-              </tr>
-              <tr>
-                <td>Servings</td>
-                <td className="fw-light">{round(servingsPerContainer, 0)}</td>
-              </tr>
-              {nutrientLabels.map((label, index) => {
-                return (
-                  <tr
-                    key={`${foodId}${label}${nutrientValues[index]}${NUTRIENTS[label].name}`}
-                  >
-                    <td>{NUTRIENTS[label].name}</td>
-                    <td className="fw-light">
-                      {`${round(nutrientValues[index], 1)} ${
-                        NUTRIENTS[label].unit
-                      }`}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+    <div className="container">
+      <div className="row rounded card shadow mb-3">
+        <span className="col-sm-auto mt-3 text-center">
+          <h4 className="">{label}</h4>
+        </span>
+        <span className="col-sm-auto mt-2">
+          <small>
+            <table className="table table-sm ">
+              <tbody className="table-striped">
+                <tr>
+                  <td scope="row">Category</td>
+                  <td className="fw-light">{category}</td>
+                </tr>
+                <tr>
+                  <td>Servings</td>
+                  <td className="fw-light">{round(servingsPerContainer, 0)}</td>
+                </tr>
+                {nutrientLabels.map((label, index) => {
+                  return (
+                    <tr
+                      key={`${foodId}${label}${nutrientValues[index]}${NUTRIENTS[label].name}`}
+                    >
+                      <td>{NUTRIENTS[label].name}</td>
+                      <td className="fw-light">
+                        {`${round(nutrientValues[index], 1)} ${
+                          NUTRIENTS[label].unit
+                        }`}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </small>
+        </span>
+        <span className="col mb-2 text-center">
+          <button className="btn btn-secondary">{`Log ${label}`}</button>
+        </span>
       </div>
-    </>
+    </div>
   );
 };
