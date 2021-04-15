34e1e0821c28eb0dfee7162acb05df26945f1f40
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-15 14:08:51 -0600

    added servings and servings per container

--- src/components/forms/ManualFoodForm.js
+++ src/components/forms/ManualFoodForm.js
@@ -15,6 +15,8 @@ export const ManualFoodForm = () => {
     label: "",
     category: "",
     categoryLabel: "",
+    servings: 1,
+    servingsPerContainer: 1,
     nutrients: NUTRIENTS,
   });
 
@@ -35,69 +37,120 @@ export const ManualFoodForm = () => {
       nutrients: { ...food.nutrients, [name]: { ...NUTRIENTS[name], value } },
     });
   };
-  const { label, category, categoryLabel, nutrients } = food;
+  const {
+    label,
+    category,
+    categoryLabel,
+    nutrients,
+    servings,
+    servingsPerContainer,
+  } = food;
 
   console.log(nutrients);
 
   return (
-    <form className="card p-2" onSubmit={handleSubmitFood}>
-      <div className="row">
-        <div className="col-sm-auto text-center">
-          <p>Food</p>
-          <hr />
+    <form className="card shadow p-2" onSubmit={handleSubmitFood}>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-auto text-center">
+            <p>Food</p>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-auto">
+            <span className="input-group mb-1">
+              <Label label="Label" name="label" inputText={true} />
+              <TextInput
+                for="label"
+                name="label"
+                placeholder="Food Name"
+                id="label"
+                value={label}
+                onChange={handleValueChange}
+              />
+            </span>
+          </div>
+          <div className="col-sm-auto">
+            <span className="input-group mb-1">
+              <Label label="Category" name="category" inputText={true} />
+              <Select
+                name="category"
+                id="category"
+                value={category}
+                onChange={handleValueChange}
+                selectOptions={CATEGORIES.map((c, i) => ({
+                  id: i,
+                  key: c.category,
+                  value: capitalize(c.category),
+                }))}
+              />
+            </span>
+          </div>
+          <div className="col-sm-auto">
+            <span className="input-group mb-1">
+              <Label label="Type" name="categoryLabel" inputText={true} />
+              <Select
+                name="categoryLabel"
+                id="categoryLabel"
+                value={categoryLabel}
+                onChange={handleValueChange}
+                selectOptions={CATEGORY_LABELS.map((c, i) => ({
+                  id: i,
+                  key: c.label,
+                  value: capitalize(c.label),
+                }))}
+              />
+            </span>
+          </div>
         </div>
       </div>
-      <div className="row mb-3">
-        <div className="col-sm-auto">
-          <span className="input-group mb-1">
-            <Label label="Label" name="label" inputText={true} />
-            <TextInput
-              for="label"
-              name="label"
-              placeholder="Food Name"
-              id="label"
-              value={label}
-              onChange={handleValueChange}
-            />
-          </span>
-        </div>
-        <div className="col-sm-auto">
-          <span className="input-group mb-1">
-            <Label label="Category" name="category" inputText={true} />
-            <Select
-              name="category"
-              id="category"
-              value={category}
-              onChange={handleValueChange}
-              selectOptions={CATEGORIES.map((c, i) => ({
-                id: i,
-                key: c.category,
-                value: capitalize(c.category),
-              }))}
-            />
-          </span>
-        </div>
-        <div className="col-sm-auto">
-          <span className="input-group">
-            <Label label="Type" name="categoryLabel" inputText={true} />
-            <Select
-              name="categoryLabel"
-              id="categoryLabel"
-              value={categoryLabel}
-              onChange={handleValueChange}
-              selectOptions={CATEGORY_LABELS.map((c, i) => ({
-                id: i,
-                key: c.label,
-                value: capitalize(c.label),
-              }))}
-            />
-          </span>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-12 text-center">
+            <p>Servings</p>
+            <hr />
+          </div>
+          <div className="col d-flex ">
+            <span className="input-group">
+              <Label label="Servings" name="servings" inputText={true} />
+              <TextInput
+                for="servings"
+                name="servings"
+                placeholder="servings"
+                id="servings"
+                value={servings}
+                onChange={handleValueChange}
+              />
+            </span>
+          </div>
+          <div className="col d-flex">
+            <span className="input-group">
+              <Label label="of" name="servingsPerContainer" inputText={true} />
+              <TextInput
+                for="servingsPerContainer"
+                name="servingsPerContainer"
+                placeholder="servingsPerContainer"
+                id="servingsPerContainer"
+                value={servingsPerContainer}
+                onChange={handleValueChange}
+              />
+            </span>
+          </div>
         </div>
       </div>
-      <NutrientsForm
-        nutrients={nutrients}
-        onNutrientChange={handleNutrientChange}
-      />
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-sm-auto text-center">
+            <p>Nutrients</p>
+            <hr />
+          </div>
+          <NutrientsForm
+            nutrients={nutrients}
+            onNutrientChange={handleNutrientChange}
+          />
+        </div>
+      </div>
       <div className="row">
         <div className="col-sm-auto text-center">
           <button className="btn btn-primary px-4" type="submit">
