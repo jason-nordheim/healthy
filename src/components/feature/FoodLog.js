2d8feaf4cac55c51a282ba637fc20ddeb742a80e
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-15 14:07:50 -0600

    simpliefied

--- src/components/feature/FoodLog.js
+++ src/components/feature/FoodLog.js
@@ -1,7 +1,21 @@
-export const FoodLog = () => {
+import { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import { getFoods } from "../../util/ApiUtils";
+import { Food } from "./Food";
+export const FoodLog = ({ loggedFoods, deleteFood }) => {
   return (
     <div>
-      <p>Food Log</p>
+      {loggedFoods?.map((food) => {
+        return (
+          <Food
+            key={food._id}
+            food={food}
+            foodActionHandler={() => deleteFood(food)}
+            foodActionLabel={"Remove"}
+          />
+        );
+      })}
     </div>
   );
 };
