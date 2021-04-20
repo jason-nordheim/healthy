de6b787a7fcb566b34c83ae60acb24cb01f7917b
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-20 11:58:32 -0600

    simplified

--- server/src/middleware/authenticate.js
+++ server/src/middleware/authenticate.js
@@ -3,30 +3,19 @@ const { jwt_opt, jwt_key } = require("../config/app.config");
 const isAfter = require("date-fns/isAfter");
 
 const authenticateUser = (req, res, next) => {
-  //console.log({ headers: req.header });
-  if (!req.headers.authorization) {
-    return res.status(400).send();
-  }
+  if (!req.headers.authorization) return res.status(400).send();
+
   const token = req.headers.authorization.split(" ")[1];
-  //console.log("token", { token });
-  try {
-    const decoded_token = jwt.verify(token, jwt_key, jwt_opt);
-    const token_expires = new Date(decoded_token.exp * 1000);
+  const decoded_token = jwt.verify(token, jwt_key, jwt_opt);
+  const token_expires = new Date(decoded_token.exp * 1000);
+  const isExpired = isAfter(Date.now, token_expires);
+  const expirationMessage = `token expired at ${token_expires}`;
 
-    if (isAfter(Date.now, token_expires)) {
-      return res
-        .status(403)
-        .json({ error: `token expired at ${token_expires}` })
-        .send();
-    }
-    req.headers.user_id = decoded_token.id;
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(403).json({ error }).send();
-  }
+  if (isExpired) return res.status(403).send(expirationMessage);
+
+  req.headers.userId = decoded_token.id;
+  next();
 };
 
-module.exports = {
-  authenticateUser,
-};
+const getUserId = (request) => request.headers.userId;
+module.exports = { authenticateUser, getUserId };
