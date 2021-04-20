81e7752f6a46d658420f0ad33c577565aec7f266
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-20 13:48:40 -0600

    rebuilt mongo client file

---
+++ server/src/config/config.mongoose.js
@@ -0,0 +1,30 @@
+const { Mongoose } = require("mongoose");
+const { mongo_url } = require("./app.config");
+
+const mongoClient = new Mongoose();
+
+mongoClient.connection.on(
+  "error",
+  console.debug.bind(console, "Connection Error:")
+);
+mongoClient.connection.once("connected", () => {
+  console.debug("Database connection established successfully");
+});
+mongoClient.connection.once("disconnected", () => {
+  console.debug("Database connection established successfully");
+});
+
+const connect = () =>
+  mongoClient.connect(mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+const disconnect = () => mongoClient.disconnect();
+
+module.exports = {
+  mongoClient,
+  connect,
+  disconnect,
+};
