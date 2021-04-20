7ae8befb9d157dae7210ee1e6dd3490e932e72b2
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-20 13:48:49 -0600

    removed mongo client

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,28 +1,13 @@
-const mongoose = require("mongoose");
-
-const { mongo_url } = require("./app.config");
-
+const { mongoClient } = require("../config/config.mongoose");
 const weightSchema = require("./schema.weight");
 const foodSchema = require("./schema.food");
 const exerciseSchema = require("./schema.exercise");
 const userSchema = require("./schema.user");
 
-const mongooseClient = new mongoose.Mongoose();
-
-mongooseClient.connect(mongo_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-const connection = mongooseClient.connection;
-connection.on("error", console.error.bind(console, "connection error:"));
-connection.once("open", () => console.info("Database Connection established"));
-
-const User = mongooseClient.model("user", userSchema);
-const Exercise = mongooseClient.model("exercise", exerciseSchema);
-const Food = mongooseClient.model("food", foodSchema);
-const Weight = mongooseClient.model("weight", weightSchema);
+const User = mongoClient.model("user", userSchema);
+const Exercise = mongoClient.model("exercise", exerciseSchema);
+const Food = mongoClient.model("food", foodSchema);
+const Weight = mongoClient.model("weight", weightSchema);
 
 module.exports = {
   User,
