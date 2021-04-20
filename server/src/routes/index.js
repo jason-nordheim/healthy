3d37662e281acf01f9b6e8561991bd222204c1fb
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-20 11:59:40 -0600

    barrelled routers to streamline importing

---
+++ server/src/routes/index.js
@@ -0,0 +1,11 @@
+const userRouter = require("./routes.user");
+const foodRouter = require("./routes.food");
+const exerciseRouter = require("./routes.exercise");
+const weightRouter = require("./routes.weight");
+
+module.exports = {
+  userRouter,
+  foodRouter,
+  exerciseRouter,
+  weightRouter,
+};
