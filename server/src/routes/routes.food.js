7b66a05a914f718f3a52d0638357dc09502eddbb
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-20 11:59:02 -0600

    created Food routes

---
+++ server/src/routes/routes.food.js
@@ -0,0 +1,52 @@
+const { Router } = require("express");
+const { Food } = require("../models");
+const { authenticateUser, getUserId } = require("../middleware/authenticate");
+const { searchFoods } = require("../util/edaman.api");
+
+const foodRouter = Router();
+
+// CREATE
+foodRouter.route("/").post(authenticateUser, (req, res) => {
+  const newFood = Food({
+    ...req.body,
+    userId: getUserId(req),
+  });
+
+  newFood
+    .save()
+    .then(() => res.status(201).send("Food saved"))
+    .catch((error) => res.status(400).send(error));
+});
+
+// READ
+foodRouter.route("/").get(authenticateUser, (req, res) => {
+  Food.find({ userId: getUserId(req) })
+    .then((foods) => res.json(foods))
+    .catch((error) => res.status(400).json(error));
+});
+
+// UPDATE
+foodRouter.route("/:id").patch(authenticateUser, (req, res) => {
+  Food.updateOne(
+    { _id: req.params.id, userId: getUserId(req) },
+    { ...req.body }
+  )
+    .then(() => res.status(200).send("Food updated"))
+    .catch((error) => res.status(400).json(error));
+});
+
+// DELETE
+foodRouter.route("/:id").patch(authenticateUser, (req, res) => {
+  Food.deleteOne({ _id: req.params.id, userId: getUserId(req) })
+    .then(() => res.status(200).send("Food deleted"))
+    .catch((error) => res.status(400).json(error));
+});
+
+// SEARCH
+foodRouter.route("/search").get((req, res) => {
+  searchFoods(req.query.query)
+    .then((result) => res.status(200).json(result.data))
+    .catch((error) => res.status(400).json(error));
+});
+
+module.exports = foodRouter;
