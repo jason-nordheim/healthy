1ab04ff515b0f3e893206864d554b5e56305b041
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-21 10:45:38 -0600

    fixed imports

--- server/src/middleware/authenticate.js
+++ server/src/middleware/authenticate.js
@@ -1,12 +1,12 @@
 var jwt = require("jsonwebtoken");
-const { jwt_opt, jwt_key } = require("../config/app.config");
+const { jwtKey, jwtOptions } = require("../config/app.config");
 const isAfter = require("date-fns/isAfter");
 
 const authenticateUser = (req, res, next) => {
   if (!req.headers.authorization) return res.status(400).send();
 
   const token = req.headers.authorization.split(" ")[1];
-  const decoded_token = jwt.verify(token, jwt_key, jwt_opt);
+  const decoded_token = jwt.verify(token, jwtKey, jwtOptions);
   const token_expires = new Date(decoded_token.exp * 1000);
   const isExpired = isAfter(Date.now, token_expires);
   const expirationMessage = `token expired at ${token_expires}`;
