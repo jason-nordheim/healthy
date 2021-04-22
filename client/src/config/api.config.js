ecd25a22f7126617a68258ffe3eb754bcd29a65d
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-22 11:36:11 -0600

    Update api.config.js

--- client/src/config/api.config.js
+++ client/src/config/api.config.js
@@ -1,53 +1,149 @@
 const BASE_SERVER_URI = `http://localhost:5000`;
 
-export const API_CONFIG = {
-  base: BASE_SERVER_URI,
+const API_CONFIG = {
   routes: {
-    auth: {
-      url: `${BASE_SERVER_URI}/api/login`,
-      method: "POST",
-    },
-    user: {
-      register: {
-        url: `${BASE_SERVER_URI}/api/users`,
-        method: "POST",
+    users: [
+      {
+        name: "get profile",
+        description: "returns user profile data ",
+        request: {
+          path: "/api/users",
+          method: "GET",
+          auth: true,
+          query: null,
+          body: null,
+        },
+        response: {
+          success: {
+            statusCode: 200,
+            body: {
+              first: String,
+              last: String,
+              email: String,
+              password: String,
+              day: Number,
+              month: Number,
+              year: Number,
+              height: Number || undefined,
+            },
+            format: "application/json",
+          },
+          clientError: {
+            statusCode: 400,
+            body: { error: String },
+            format: "application/json",
+          },
+          serverError: {
+            statusCode: 500,
+            body: { error: String },
+            format: "application/json",
+          },
+        },
       },
-      getInfo: {
-        url: `${BASE_SERVER_URI}/api/users`,
-        method: "GET",
+      {
+        name: "register",
+        description: "creates a user account",
+        request: {
+          path: "/api/users",
+          method: "POST",
+          auth: false,
+          query: null,
+          body: {
+            first: String,
+            last: String,
+            email: String,
+            password: String,
+            day: Number,
+            month: Number,
+            year: Number,
+            height: Number || undefined,
+          },
+        },
+        response: {
+          success: {
+            statusCode: 201,
+            body: "User registered",
+            format: "application/json",
+          },
+          clientError: {
+            statusCode: 400,
+            body: { error: String },
+            format: "application/json",
+          },
+          serverError: {
+            statusCode: 500,
+            body: { error: String },
+            format: "application/json",
+          },
+        },
       },
-      updateInfo: {
-        url: `${BASE_SERVER_URI}/api/users`,
-        method: "PATCH",
+      {
+        name: "Delete profile",
+        description: "Deletes profile associated with bearer token",
+        request: {
+          path: "/api/users",
+          method: "PATCH",
+          auth: true,
+          query: null,
+          body: {
+            first: String || undefined,
+            last: String || undefined,
+            email: String || undefined,
+            password: String || undefined,
+            day: Number || undefined,
+            month: Number || undefined,
+            year: Number || undefined,
+            height: Number || undefined,
+          },
+        },
+        response: {
+          success: {
+            statusCode: 201,
+            body: "User updated",
+            format: "application/json",
+          },
+          clientError: {
+            statusCode: 400,
+            body: { error: String },
+            format: "application/json",
+          },
+          serverError: {
+            statusCode: 500,
+            body: { error: String },
+            format: "application/json",
+          },
+        },
       },
-    },
-    weight: {
-      add: {
-        url: `${BASE_SERVER_URI}/api/weights`,
-        method: "POST",
+      {
+        name: "delete profile",
+        description: "returns user profile data ",
+        request: {
+          path: "/api/users",
+          method: "DELETE",
+          auth: true,
+          query: null,
+          body: null,
+        },
+        response: {
+          success: {
+            statusCode: 200,
+            body: "User deleted",
+            format: "application/json",
+          },
+          clientError: {
+            statusCode: 400,
+            body: { error: String },
+            format: "application/json",
+          },
+          serverError: {
+            statusCode: 500,
+            body: { error: String },
+            format: "application/json",
+          },
+        },
       },
-      getAll: {
-        url: `${BASE_SERVER_URI}/api/weights`,
-        method: "GET",
-      },
-      deleteOne: {
-        url: `${BASE_SERVER_URI}/api/weights`,
-        method: "DELETE",
-      },
-    },
-    food: {
-      search: {
-        url: `${BASE_SERVER_URI}/api/foods/search`,
-        method: "GET",
-      },
-      addFood: {
-        url: `${BASE_SERVER_URI}/api/foods`,
-        method: "POST",
-      },
-      getFoods: {
-        url: `${BASE_SERVER_URI}/api/foods`,
-        method: "GET",
-      },
-    },
+    ],
+    foods: [],
+    weights: [],
   },
 };
