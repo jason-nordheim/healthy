4335492527dfad893cbf8cdd4257a6631263fbbd
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 11:15:25 -0600

    barreled pages into index file for easy export

---
+++ client/src/components/pages/index.js
@@ -0,0 +1,6 @@
+import { AccountPage } from "./accounts";
+import { HomePage } from "./home";
+import { ToolsPage } from "./tools";
+import { LogsPage } from "./logs";
+
+export { AccountPage, HomePage, ToolsPage, LogsPage };
