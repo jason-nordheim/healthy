6537cfada81f7652813fa6273696072d95fb62df
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 11:15:09 -0600

    created dedicated directories for each page

---
+++ client/src/components/pages/accounts/index.js
@@ -0,0 +1,10 @@
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+export const AccountPage = () => {
+  return (
+    <div className="container">
+      <Register />
+    </div>
+  );
+};
