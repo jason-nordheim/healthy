6ec5a3f39d643365e1e7d08b1bd99812ab71416a
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 17:42:07 -0600

    started stubbing out register action

--- client/src/context/context.auth.js
+++ client/src/context/context.auth.js
@@ -1,6 +1,10 @@
 import { createContext, useContext, useReducer } from "react";
-import { authReducer, initialAuthState } from "../reducers";
+import { authActions, authReducer, initialAuthState } from "../reducers";
+import { API_ROUTES } from "../config";
+import axios from "axios";
 export const AuthContext = createContext();
+
+const { registerRequest, registerFailure } = authActions;
 
 export const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialAuthState);
@@ -8,7 +12,19 @@ export const AuthProvider = ({ children }) => {
   // actions
   const login = (user) => {};
   const logout = () => {};
-  const register = (user) => {};
+  const register = async (user) => {
+    // starting to parse
+    dispatch(registerRequest());
+    try {
+      const response = await axios.post(API_ROUTES.user.createUser.url, {
+        ...user,
+      });
+      console.log({ response });
+    } catch (error) {
+      console.log({ error });
+      console.log({ data: error.response.data });
+    }
+  };
 
   const value = { state, login, logout, register };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
