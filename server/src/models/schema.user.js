76b485da0794f3d0fc51b0314d9b9904c358c3dd
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 12:08:35 -0600

    updated user schema to store birthday as a string and added username field

--- server/src/models/schema.user.js
+++ server/src/models/schema.user.js
@@ -4,10 +4,15 @@ const userSchema = new Schema(
   {
     first: { type: String, required: true }, // first name
     last: { type: String, required: true }, // last name
-    day: { type: Number, required: true }, // birth day
-    month: { type: Number, required: true }, // birth month
-    year: { type: Number, required: true }, // birth year
     height: { type: Number, required: false }, // height in centimeters
+    uom: { type: String, required: true, default: "imperial" },
+    birthday: {
+      type: String,
+      required: true,
+      validate: (val) => {
+        return /(\d{4})-(\d{1,2})-(\d{1,2})/.test(val);
+      },
+    },
     email: {
       type: String,
       required: true,
@@ -16,7 +21,12 @@ const userSchema = new Schema(
         return /^[^@]+@\w+(\.\w+)+\w$/.test(val);
       },
     },
-    uom: { type: String, required: true, default: "imperial" },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 5,
+    },
     passwordDigest: { type: String, required: true },
   },
   { timestamps: true }
