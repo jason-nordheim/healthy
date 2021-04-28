3f9c35b62294bd547cff2dbdefe7e93959cdf638
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 17:41:54 -0600

    fixed issue where the navlinks were not highlighting correctly

--- client/src/components/pages/accounts/UnAuthenticated.js
+++ client/src/components/pages/accounts/UnAuthenticated.js
@@ -4,29 +4,40 @@ import {
   useRouteMatch,
   NavLink,
   Redirect,
+  useLocation,
 } from "react-router-dom";
 import { Login } from "./Login";
 import { Register } from "./Register";
 
 export const UnAuthenticated = () => {
   let { path, url } = useRouteMatch();
+  let location = useLocation();
+  const loginPath = `${url}/login`;
+  const registerPath = `${url}/register`;
+  console.log({ location, loginPath });
   return (
     <>
       <div className="row">
         <div className="btn-group" role="group" aria-label="Login or register">
           <NavLink
             type="button"
-            to={`${url}/login`}
-            activeClassName="btn btn-primary"
-            className="btn border"
+            to={loginPath}
+            className={
+              /login/.test(location.pathname)
+                ? "btn btn-primary border"
+                : "btn btn-secondary border"
+            }
           >
             Login
           </NavLink>
           <NavLink
             type="button"
-            to={`${url}/register`}
-            activeClassName="btn btn-primary"
-            className="btn border"
+            to={registerPath}
+            className={
+              /register/.test(location.pathname)
+                ? "btn btn-primary border"
+                : "btn btn-secondary border"
+            }
           >
             Register
           </NavLink>
