0c4d637556de7711b215139a652ca2a312075446
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-28 11:18:28 -0600

    redefined App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,17 @@
-import { useReducer } from "react";
-import { Route } from "react-router";
-import { AuthContext, AuthReducer } from "../context/auth.context";
-import { AuthState } from "../context/auth.state";
-import { AppRoutes } from "../config/routes";
-import { NavBar } from "./layout/nav/NavBar";
-
+import { Route } from "react-router-dom";
+import { Navbar } from "./layout";
+import { AuthProvider } from "../context";
+import { AppRoutes } from "../config";
 export const App = () => {
-  const [authState, authDispatch] = useReducer(
-    AuthReducer,
-    AuthState.initial()
-  );
-
   return (
-    <AuthContext.Provider value={[authState, authDispatch]}>
-      <NavBar />
-      {AppRoutes.map((route) => (
-        <Route
-          key={route.path}
-          exact={route.exact}
-          path={route.path}
-          render={route.component}
-        />
-      ))}
-    </AuthContext.Provider>
+    <AuthProvider>
+      <Navbar />
+      {AppRoutes.map((route) => {
+        const { component, exact, path } = route;
+        return (
+          <Route key={path} path={path} render={component} exact={exact} />
+        );
+      })}
+    </AuthProvider>
   );
 };
