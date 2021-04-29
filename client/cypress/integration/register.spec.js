5c416d084f1116d0a5521f4667dc8b0b0f1efc68
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-29 11:15:42 -0600

    added test to validate that error message is displayed after submitting incorrect values for password

--- client/cypress/integration/register.spec.js
+++ client/cypress/integration/register.spec.js
@@ -11,6 +11,9 @@
 import { createTestUser } from "../helpers";
 
 describe("Unauthenticated User", () => {
+  beforeEach(() => {
+    cy.clearLocalStorage(); // make sure we don't have any saved tokens
+  });
   it("Can directly navigate to Accounts Page", () => {
     cy.visit("http://localhost:3000/account");
   });
@@ -96,7 +99,7 @@ describe("Unauthenticated User", () => {
       .should("not.have.attr", "disabled");
   });
 
-  it.only("redirects to login when the form is filled in and submitted", () => {
+  it("redirects to login when the form is filled in and submitted", () => {
     cy.visit("http://localhost:3000/account");
     cy.url().should("match", /login/);
     cy.get("div.btn-group")
@@ -105,7 +108,6 @@ describe("Unauthenticated User", () => {
       .click();
 
     const testUser = createTestUser();
-    console.log(testUser);
 
     // fill in the fields
     cy.get("input#first").type(testUser.first);
@@ -129,5 +131,37 @@ describe("Unauthenticated User", () => {
 
     cy.get("div.container").contains(/Welcome/i);
   });
-  it.skip("displays error message if submitted with incorrect values", () => {});
+  it.only("displays error message if submitted with incorrect values", () => {
+    cy.visit("http://localhost:3000/account");
+    cy.url().should("match", /login/);
+    cy.get("div.btn-group")
+      .contains(/register/i)
+      .should("have.class", "btn-secondary")
+      .click();
+
+    const testUser = createTestUser();
+
+    // fill in the fields
+    cy.get("input#first").type(testUser.first);
+    cy.get("input#last").type(testUser.last);
+    cy.get("input#birthday").invoke("val", testUser.birthday).trigger("change");
+
+    cy.get("input#email").type(testUser.email);
+    cy.get("input#username").type(testUser.username);
+    cy.get("input#password").type(testUser.password.slice(0, 3));
+
+    // button no longer disabled
+    cy.get("div.col.text-center")
+      .contains(/register/i)
+      .should("not.have.attr", "disabled");
+
+    // click the button
+    cy.get("div.col.text-center")
+      .contains(/register/i)
+      .click();
+
+    cy.wait(500);
+
+    cy.get("div.container").contains(/Register/i);
+  });
 });
