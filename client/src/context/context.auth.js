47cb8e8cd58e44069703ccf0065ad0c98ad45e60
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-29 10:47:21 -0600

    completed register and login actions for happy path

--- client/src/context/context.auth.js
+++ client/src/context/context.auth.js
@@ -4,29 +4,67 @@ import { API_ROUTES } from "../config";
 import axios from "axios";
 export const AuthContext = createContext();
 
-const { registerRequest, registerFailure } = authActions;
+const {
+  registerRequest,
+  registerFailure,
+  registerSuccess,
+  loginRequest,
+  loginSuccess,
+  loginFailure,
+} = authActions;
 
 export const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialAuthState);
 
   // actions
-  const login = (user) => {};
-  const logout = () => {};
-  const register = async (user) => {
-    // starting to parse
-    dispatch(registerRequest());
+  const login = async (user) => {
+    const { url } = API_ROUTES.user.login;
+    dispatch(loginRequest());
     try {
-      const response = await axios.post(API_ROUTES.user.createUser.url, {
-        ...user,
-      });
+      const response = await axios.post(url, { ...user });
+      if (response.status === 201 && response.data?.token) {
+        dispatch(loginSuccess(response.data.token));
+      } else {
+        const info = JSON.stringify(response.data);
+        dispatch(loginFailure("Failed to login: " + info));
+      }
       console.log({ response });
     } catch (error) {
       console.log({ error });
-      console.log({ data: error.response.data });
     }
   };
+  const logout = () => {};
+  const register = async (user) => {
+    const { url } = API_ROUTES.user.createUser;
+    // starting to parse
+    dispatch(registerRequest());
+    try {
+      const response = await axios.post(url, { ...user });
 
-  const value = { state, login, logout, register };
+      if (response.status === 201) {
+        dispatch(registerSuccess());
+        // pause a moment to let the transaction finish
+        setTimeout(200, login(user));
+      }
+    } catch (error) {
+      // something went wrong
+      if (error.response.data.code === 11000) {
+        // duplicate key, loop through displaying which keys have been use
+        let message = "Error: ";
+        const errorObjKeys = Object.keys(error.response.data.keyPattern);
+        errorObjKeys.forEach(
+          (errorKey) => (message += ` ${errorKey} already used.`)
+        );
+        dispatch(registerFailure(message));
+      } else {
+        // unknown error
+        const details = JSON.stringify(error.response.data);
+        dispatch(registerFailure("Unknown Error: " + details));
+      }
+    }
+  };
+  const actions = { login, logout, register };
+  const value = { state, actions };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
