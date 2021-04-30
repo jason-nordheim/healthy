dbb3570728eca17226818e71d8511dc8443d8709
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-29 18:30:15 -0600

    removed old front-end files

--- client/src/archive/context/user.type.js
+++
@@ -1,5 +0,0 @@
-export const USER_TYPE = {
-  PROFILE_REQUEST: "PROFILE_REQUEST",
-  PROFILE_SUCCESS: "PROFILE_SUCCESS",
-  PROFILE_FAILURE: "PROFILE_FAILURE", // failed to retrieve profile information
-};
