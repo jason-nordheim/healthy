17a14119c496e842e3fe1db097f434334553fca0
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-29 18:31:15 -0600

    fixed `createTestUser` function
    -ensure that first and last name are included in the username and that it is always more than 5 characters.

--- client/cypress/helpers/index.js
+++ client/cypress/helpers/index.js
@@ -1,4 +1,5 @@
 import { internet, name, date, datatype } from "faker";
+import { convertDateToString } from "../../src/helpers";
 
 const dateRegex = new RegExp(
   /(19\d{2}|20\d{2})-(0?1{1}[1-2]{1}|\d{1})-(0?[1-2]{1}[0-9]{1}|[3]{1}[0,1]{1}|[1-9]{1})/
@@ -7,20 +8,19 @@ const dateRegex = new RegExp(
 const getRandomDateString = () => {
   const yearsOld = datatype.number({ min: 17, max: 80, precision: 1 });
   const randDate = date.past(yearsOld);
-  const year = randDate.getFullYear(),
-    month = randDate.getMonth(),
-    day = randDate.getDay();
-  const monthStr = month < 10 ? `0${month}` : `${month}`;
-  const dayStr = day < 10 ? `0${day}` : `${day}`;
-  return `${year}-${monthStr}-${dayStr}`;
+  return convertDateToString(randDate);
 };
 
-const createTestUser = () => ({
-  first: name.firstName(),
-  last: name.lastName(),
-  username: internet.userName(),
-  email: internet.email(),
-  password: internet.password(),
-  birthday: getRandomDateString(),
-});
+const createTestUser = () => {
+  const first = name.firstName();
+  const last = name.lastName();
+  return {
+    first,
+    last,
+    username: internet.userName(first, last),
+    email: internet.email(),
+    password: internet.password(),
+    birthday: getRandomDateString(),
+  };
+};
 export { createTestUser };
